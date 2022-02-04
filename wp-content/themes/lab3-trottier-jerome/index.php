4ab237cfacbb9794f5f44ea3e25b7a526d68156c
Jérôme Trottier <jerome.trottier@hotmail.ca>
2022-02-04 15:34:13 -0500

    Réglé le positionnement de la sidebar ainsi que le footer

--- wp-content/themes/lab3-trottier-jerome/index.php
+++ wp-content/themes/lab3-trottier-jerome/index.php
@@ -12,13 +12,13 @@
                         <?php
                         $titre = get_the_title();
                         $titreFiltreCours = substr($titre, 7, -6);
-                        $heuresCours = substr($titre, -6);
+                        $nbHeures = substr($titre, -6);
                         $sigleCours = substr($titre, 0, 7);
                         $descCours = get_the_excerpt();
                         ?>
                         <h3 class="cours__titre"> <?= $titreFiltreCours; ?></h3>
-                        <div class="cours__nbre-heure"><?= $heuresCours; ?></div>
-                        <p class="cours__sigle"><?= $sigleCours; ?></p>
+                        <div class="cours__nbre-heure"><?= $nbHeures; ?></div>
+                        <p class="cours__sigle"><?= $sigleCours; ?> </p>
                         <p class="cours__desc"> <?= $descCours; ?></p>
                     </article>
             <?php
