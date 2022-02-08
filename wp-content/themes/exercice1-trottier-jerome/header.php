27c124965b57f50897c615d478f7240d55acc2c9
Jérôme Trottier <jerome.trottier@hotmail.ca>
2022-02-08 11:52:13 -0500

    Ajout d'un menu burger prototype

--- wp-content/themes/exercice1-trottier-jerome/header.php
+++ wp-content/themes/exercice1-trottier-jerome/header.php
@@ -20,5 +20,11 @@
         </h2>
     </header>
     <section class="site__barre">
+        <input type="checkbox" id="chk-burger">
+        <label for="chk-burger" id="burger">
+            <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" color="#000">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+            </svg>
+        </label>
         <?php wp_nav_menu(array("menu" => "principal", 'container' => "nav")); ?>
     </section>
