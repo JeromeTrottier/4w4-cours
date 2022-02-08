79ef83919b8786902e86eea17d074f817de1f7e2
Jérôme Trottier <jerome.trottier@hotmail.ca>
2022-02-08 09:35:39 -0500

    Debut exercice 1

---
+++ wp-content/themes/exercice1-trottier-jerome/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab4 Trottier Jérôme</title>
+    <?php wp_head() ?>
+</head>
+
+<body <?php body_class("site") ?>>
+    <header class="site__header">
+        <h1 class="header__titre">
+            <?php blogInfo('name') ?>
+        </h1>
+        <h2 class="header__desc">
+            <?php blogInfo('description') ?>
+        </h2>
+    </header>
+    <section class="site__barre">
+        <nav class="navigation">
+            <a href="#" class="navigation__lien">mon lien 001</a>
+            <a href="#" class="navigation__lien">mon lien 002</a>
+            <a href="#" class="navigation__lien">mon lien 003</a>
+            <a href="#" class="navigation__lien">mon lien 004</a>
+            <a href="#" class="navigation__lien">mon lien 005</a>
+            <a href="#" class="navigation__lien">mon lien 006</a>
+            <a href="#" class="navigation__lien">mon lien 007</a>
+            <a href="#" class="navigation__lien">mon lien 008</a>
+            <a href="#" class="navigation__lien">mon lien 009</a>
+            <a href="#" class="navigation__lien">mon lien 010</a>
+        </nav>
+    </section>
