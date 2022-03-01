4e1553c91708fbd3b4d5645870a7b5f01dffc9ca
Jérôme Trottier <jerome.trottier@hotmail.ca>
2022-03-01 12:54:18 -0500

    Ajout de la barre.scss et ajustement de liquid design

--- wp-content/themes/exercice2-trottier-jerome/front-page.php
+++ wp-content/themes/exercice2-trottier-jerome/front-page.php
@@ -1,8 +1,6 @@
 <?php get_header() ?>
 <main class="site__main">
     <div class="main__frontpage">
-        <div class="bloc-bleu-frontpage"></div>
-        <div class="bloc-orange-frontpage"></div>
         <?php
         if (have_posts()) :
             while (have_posts()) :
