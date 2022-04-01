3606bbebed06845675c29d911976d56075033b2e
Jérôme Trottier <jerome.trottier@hotmail.ca>
2022-04-01 15:07:27 -0400

    ajout de templates pour les cours et les macrames

--- wp-content/themes/lab5-trottier-jerome/category.php
+++ wp-content/themes/lab5-trottier-jerome/category.php
@@ -2,31 +2,20 @@
 <main class="principal">
     <?php wp_nav_menu(array(
         "menu" => "categorie_menu",
+        "container" => "nav",
     )) ?>
     <section class="formation">
         <h2 class="formation__titre">Liste des cours du programme TIM</h2>
         <div class="formation__liste">
             <?php if (have_posts()) :
                 while (have_posts()) : the_post(); ?>
-                    <?php $categories = get_the_category(); ?>
-                    <article class="formation__cours <?php echo $categories[1]->slug ?>">
-                        <?php
-                        $titre = get_the_title();
-                        $titreFiltreCours = substr($titre, 7, -6);
-                        $nbHeures = substr($titre, -6);
-                        $sigleCours = substr($titre, 0, 7);
-                        $descCours = get_the_excerpt();
-                        ?>
-                        <?php the_post_thumbnail("thumbnail"); ?>
-                        <h3 class="cours__titre">
-                            <a href="<?php echo get_permalink(); ?>">
-                                <?= $titreFiltreCours; ?>
-                            </a>
-                        </h3>
-                        <div class="cours__nbre-heure"><?= $nbHeures; ?></div>
-                        <p class="cours__sigle"><?= $sigleCours; ?> </p>
-                        <p class="cours__desc"> <?= $descCours; ?></p>
-                    </article>
+                    <?php
+                    if (is_category(array('cours', 'web', 'jeu', 'design', 'utilitaire', 'video', 'creation-3d'))) :
+                        get_template_part('gabarit/content', 'cours');
+                    else :
+                        get_template_part('gabarit/content', 'macrame');
+                    endif;
+                    ?>
                 <?php endwhile ?>
             <?php endif ?>
         </div>
